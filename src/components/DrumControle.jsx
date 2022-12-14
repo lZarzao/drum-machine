aff783d1e7c3712a640f37b0a02185f5875a571c
lZarzao <luiszarzao@icloud.com>
2022-12-14 10:49:42 -0300

    last css upgrade

--- src/components/DrumControle.jsx
+++ src/components/DrumControle.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faVolumeHigh } from '@fortawesome/free-solid-svg-icons';
 
 const DrumControle = ({
   volume,
@@ -11,19 +13,33 @@ const DrumControle = ({
 }) => {
   return (
     <div className='control'>
-      <button className='onOff' onClick={onOff}> Turn {power ? 'OFF' : 'ON'}</button>
-      <h2>Volume: %{Math.round(volume * 100)}</h2>
-      <input
-        max='1'
-        min='0'
-        step='0.01'
-        type='range'
-        value={volume}
-        onChange={handleVolumeChange}
-      />
-      <h2>Mode: {groupType}</h2>
-      <h2 id='display'>{name}</h2>
-      <button onClick={changeSoundGroup}>Change Sound Group</button>
+      <button className={`onOff ${power ? '' : 'isOff'}`} onClick={onOff}>
+        {' '}
+        Turn {power ? 'OFF' : 'ON'}
+      </button>
+      <div className='displayContainer'>
+        <h2 id='display'>{name ? name : '---'}</h2>
+        <div className='displayContainerInfo'>
+          <h2>
+            Volume: {Math.round(volume * 100)}%
+          </h2>
+          <h2>Mode: {groupType}</h2>
+        </div>
+      </div>
+      <div className='controlControler'>
+        <div>
+          <FontAwesomeIcon icon={faVolumeHigh} />
+          <input
+            max='1'
+            min='0'
+            step='0.01'
+            type='range'
+            value={volume}
+            onChange={handleVolumeChange}
+          />
+        </div>
+        <button className='changeMode' onClick={changeSoundGroup}>Change Mode</button>
+      </div>
     </div>
   );
 };
